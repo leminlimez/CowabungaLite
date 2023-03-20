b2c9e0942114ef9ceaeb2ae087fad1ca3403aaec
Avangelista <rory.madden8@gmail.com>
2023-03-20 17:28:46 +1100

    footnote working

--- CowabungaJailed/RootView.swift
+++ CowabungaJailed/RootView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct RootView: View {
+    @State private var devices: [Device]?
+    @State private var selectedDeviceIndex = 0
     @State var homeActive: Bool = true
+    
+    func updateDevices() {
+        devices = getDevices()
+        if selectedDeviceIndex >= (devices?.count ?? 0) {
+            selectedDeviceIndex = 0
+        }
+    }
+    
     var body: some View {
         NavigationView {
             List {
+                HStack {
+                    Picker(selection: $selectedDeviceIndex, label: Image(systemName: "iphone")) {
+                        if let devices = devices {
+                            if devices.isEmpty {
+                                Text("None").tag(0)
+                            } else {
+                                ForEach(0..<devices.count, id: \.self) { index in
+                                    Text("\(devices[index].name) (\(devices[index].uuid))")
+                                }
+                            }
+                        } else {
+                            Text("Loading...")
+                        }
+                    }.onAppear {
+                        updateDevices()
+                    }
+                    
+                    Button(action: {
+                        updateDevices()
+                    }) {
+                        Image(systemName: "arrow.clockwise")
+                    }
+                }
+                
+                Divider()
+                
                 NavigationLink(destination: HomeView(), isActive: $homeActive) {
                     HStack {
                         Image(systemName: "house")
@@ -54,8 +90,33 @@ struct RootView: View {
                             .padding(.horizontal, 8)
                     }
                 }
+                NavigationLink(destination: StatusBarView()) {
+                    HStack {
+                        Image(systemName: "iphone")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.blue)
+                        Text("Footnote")
+                            .padding(.horizontal, 8)
+                    }
+                }
+                
+                Divider()
+                
+                NavigationLink(destination: StatusBarView()) {
+                    HStack {
+                        Image(systemName: "checkmark.circle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.blue)
+                        Text("Apply")
+                            .padding(.horizontal, 8)
+                    }
+                }
             }
-            .frame(width: 150)
+            .frame(minWidth: 250)
         }
 //        TabView {
 //            HomeView()
