75b701653a6d469e5de94cf5536ba81538155937
Avangelista <rory.madden8@gmail.com>
2023-03-20 17:28:46 +1100

    footnote working

--- CowabungaJailed/Utilities.swift
+++ CowabungaJailed/Utilities.swift
@@ -6,6 +6,17 @@
 //
 
 import Foundation
+import AppKit
+
+class Logger: ObservableObject {
+    static let shared = Logger()
+    
+    @Published var logText = ""
+
+    func logMe(_ message: String) {
+        logText += "\(message)\n"
+    }
+}
 
 func shell(_ scriptURL: URL, arguments: [String] = [], workingDirectory: URL? = nil) throws {
     let task = Process()
@@ -23,8 +34,58 @@ func shell(_ scriptURL: URL, arguments: [String] = [], workingDirectory: URL? = 
     try task.run()
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     if let output = String(data: data, encoding: .utf8) {
-        print(output)
+        Logger.shared.logMe(output)
     }
+}
+
+func execute(_ execURL: URL, arguments: [String] = [], workingDirectory: URL? = nil) throws {
+    let task = Process()
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.standardError = pipe
+    
+    let bundlePath = Bundle.main.bundlePath
+    let frameworksPath = (bundlePath as NSString).appendingPathComponent("Contents/Frameworks")
+    let environment = ["DYLD_LIBRARY_PATH": frameworksPath]
+    task.environment = environment
+
+    task.executableURL = execURL
+    let scriptArguments = arguments.joined(separator: " ")
+    task.arguments = arguments
+    if let workingDirectory = workingDirectory {
+        task.currentDirectoryURL = workingDirectory
+    }
+    
+    try task.run()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if let output = String(data: data, encoding: .utf8) {
+        Logger.shared.logMe(output)
+    }
+}
+
+func execute2(_ execURL: URL, arguments: [String] = [], workingDirectory: URL? = nil) throws -> String {
+    let task = Process()
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.standardError = pipe
+    
+    let bundlePath = Bundle.main.bundlePath
+    let frameworksPath = (bundlePath as NSString).appendingPathComponent("Contents/Frameworks")
+    let environment = ["DYLD_LIBRARY_PATH": frameworksPath]
+    task.environment = environment
+
+    task.executableURL = execURL
+    task.arguments = arguments
+    if let workingDirectory = workingDirectory {
+        task.currentDirectoryURL = workingDirectory
+    }
+    
+    try task.run()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if let output = String(data: data, encoding: .utf8) {
+        return output
+    }
+    return ""
 }
 
 func printDirectoryTree(at path: URL, level: Int) {
@@ -35,30 +96,72 @@ func printDirectoryTree(at path: URL, level: Int) {
         let contents = try fileManager.contentsOfDirectory(at: path, includingPropertiesForKeys: nil, options: [])
         for url in contents {
             let isDirectory = (try? url.resourceValues(forKeys: [.isDirectoryKey]))?.isDirectory ?? false
-            print(prefix + url.lastPathComponent)
+            Logger.shared.logMe(prefix + url.lastPathComponent)
             if isDirectory {
                 printDirectoryTree(at: url, level: level + 1)
             }
         }
     } catch {
-        print(error.localizedDescription)
+        Logger.shared.logMe(error.localizedDescription)
     }
 }
 
-func copyFolderFromBundleToDocuments() {
-    guard let docsFolderURL = Bundle.main.url(forResource: "Docs", withExtension: nil) else {
-        fatalError("Unable to find Docs folder in app bundle")
+func copyFolderFromBundleToDocuments() -> Bool {
+    guard let docsFolderURL = Bundle.main.url(forResource: "Files", withExtension: nil) else {
+        Logger.shared.logMe("Can't find Bundle URL?")
+        return false
     }
 
     let fileManager = FileManager.default
-    guard let documentsURL = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) else { return }
-    let destinationURL = documentsURL.appendingPathComponent("Docs")
+    guard let documentsURL = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) else {
+        Logger.shared.logMe("Can't find Documents URL?")
+        return false
+    }
+    let destinationURL = documentsURL.appendingPathComponent("Files")
+
+    if fileManager.fileExists(atPath: destinationURL.path) {
+        do {
+            try fileManager.removeItem(at: destinationURL)
+            Logger.shared.logMe("Successfully removed existing Files folder from Documents directory")
+        } catch {
+            Logger.shared.logMe("Error removing existing Files folder: \(error)")
+            return false
+        }
+    }
 
     do {
         try fileManager.copyItem(at: docsFolderURL, to: destinationURL)
-        print("Successfully copied Docs folder to Documents directory")
+        Logger.shared.logMe("Successfully copied Files folder to Documents directory")
     } catch {
-        print("Error copying Docs folder: \(error)")
+        Logger.shared.logMe("Error copying Files folder: \(error)")
+        return false
+    }
+    
+    return true
+}
+
+struct Device {
+    let uuid: String
+    let name: String
+}
+
+func getDevices() -> [Device] {
+    guard let exec = Bundle.main.url(forResource: "idevice_id", withExtension: "") else { return [] }
+    let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    do {
+        let devices = try execute2(exec, arguments:["-l"], workingDirectory: documentsDirectory) // array of UUIDs
+        let devicesArr = devices.split(separator: "\n", omittingEmptySubsequences: true)
+        
+        var deviceStructs: [Device] = []
+        for d in devicesArr {
+            guard let exec2 = Bundle.main.url(forResource: "idevicename", withExtension: "") else { continue }
+            let deviceName = try execute2(exec2, arguments:["-u", String(d)], workingDirectory: documentsDirectory)
+            let device = Device(uuid: String(d), name: deviceName)
+            deviceStructs.append(device)
+        }
+        return deviceStructs
+    } catch {
+        return []
     }
 }
 
