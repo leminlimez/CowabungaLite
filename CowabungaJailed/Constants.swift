b9aa254c3d054395ea4546d9c5c3841eadff4cd4
Avangelista <rory.madden8@gmail.com>
2023-04-21 16:29:03 +1000

    split some files + added function for getting apps and icons

--- CowabungaJailed/Constants.swift
+++ CowabungaJailed/Constants.swift
@@ -9,3 +9,36 @@ import Foundation
 
 let fm = FileManager.default
 @usableFromInline let documentsDirectory = fm.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("com.leemin.CowabungaJailed")
+
+class AppInfo {
+    let bundleId: String
+    let name: String
+    let oldWebclipExists: Bool
+    let icon: Data?
+    let themedIcon: Data?
+
+    init(bundleId: String, name: String, oldWebclipExists: Bool, icon: Data?, themedIcon: Data?) {
+        self.bundleId = bundleId
+        self.name = name
+        self.oldWebclipExists = oldWebclipExists
+        self.icon = icon
+        self.themedIcon = themedIcon
+    }
+}
+
+struct Device {
+    let uuid: String
+    let name: String
+    let version: String
+    let ipad: Bool
+}
+
+enum Tweak: String {
+    case footnote = "Footnote"
+    case statusBar = "StatusBar"
+    case springboardOptions = "SpringboardOptions"
+    case skipSetup = "SkipSetup"
+    case themes = "AppliedTheme"
+    case dynamicIsland = "DynamicIsland"
+    case none = "None"
+}
---
+++ CowabungaJailed/Logger.swift
@@ -0,0 +1,20 @@
+//
+//  Logger.swift
+//  CowabungaJailed
+//
+//  Created by Lauren Woo on 21/4/2023.
+//
+
+import Foundation
+
+@objc class Logger: NSObject, ObservableObject {
+    @objc static let shared = Logger()
+    
+    @Published var logText = ""
+
+    @objc func logMe(_ message: String) {
+        logText += "\(message)\n"
+    }
+}
+
+
