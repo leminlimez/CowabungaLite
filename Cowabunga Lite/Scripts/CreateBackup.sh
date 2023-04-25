49aa579e4d0454004dc6d8dc9b5252b045b130e4
Avangelista <rory.madden8@gmail.com>
2023-04-25 18:16:05 +1000

    fix file sizes for backup generator

--- Cowabunga Lite/Scripts/CreateBackup.sh
+++ Cowabunga Lite/Scripts/CreateBackup.sh
@@ -47,8 +47,7 @@ for domain in "$1"/*; do
 				printf $fileHash | xxd -r -p >>$output_file
 				printf "FFFF81FF0000000000000000000001F5000001F5" | xxd -r -p >>$output_file
 				printf "%08x%08x%08x" $RANDOM$RANDOM $RANDOM$RANDOM $RANDOM$RANDOM | xxd -r -p >>$output_file
-				printf "000000000000" | xxd -r -p >>$output_file
-				printf "%04x" $(stat -f %z "$file") | xxd -r -p >>$output_file
+				printf "%016x" $(stat -f %z "$file") | xxd -r -p >>$output_file
 				printf "0400" | xxd -r -p >>$output_file
 
 				printf " - written structure to Manifest.mbdb"
