a080a620025ce28429a35fababdbdffc2145f9cf
leminlimez <jirizarry317@gmail.com>
2023-06-06 16:10:55 -0400

    fix files not compiling

--- Package.swift
+++ Package.swift
@@ -25,9 +25,7 @@ let package = Package(
                 "CowabungaLiteApp.swift",
                 "ControlCenterPresets",
                 "CLI_Only.swift",
-                "Extensions/NSImage++.swift",
-                "Files/SpringboardOptions/SysSharedContainerDomain-systemgroup.com.apple.configurationprofiles",
-                "Files/SkipSetup/SysSharedContainerDomain-systemgroup.com.apple.configurationprofiles" // these files are causing issues due to the length of the folder name
+                "Extensions/NSImage++.swift"
             ],
             resources: [
                 .process("Windows_Scripts"),
