0b9dbeb8d919aba454e7bb3d7cc797b07f514804
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-06-30 23:23:40 -0400

    update libi - NEEDS FIXING

--- Cowabunga Lite/Libraries/patchDylibs.sh
+++ Cowabunga Lite/Libraries/patchDylibs.sh
@@ -1,20 +1,20 @@
 install_name_tool -id @executable_path/../Frameworks/libimobiledevice-1.0.6.dylib libimobiledevice-1.0.6.dylib
 install_name_tool -id @executable_path/../Frameworks/libusbmuxd-2.0.6.dylib libusbmuxd-2.0.6.dylib
 install_name_tool -id @executable_path/../Frameworks/libimobiledevice-glue-1.0.0.dylib libimobiledevice-glue-1.0.0.dylib
-install_name_tool -id @executable_path/../Frameworks/libplist-2.0.3.dylib libplist-2.0.3.dylib
+install_name_tool -id @executable_path/../Frameworks/libplist-2.0.4.dylib libplist-2.0.4.dylib
 install_name_tool -change /usr/local/lib/libimobiledevice-1.0.6.dylib @executable_path/../Frameworks/libimobiledevice-1.0.6.dylib libimobiledevice-1.0.6.dylib
 install_name_tool -change /usr/local/lib/libimobiledevice-1.0.6.dylib @executable_path/../Frameworks/libimobiledevice-1.0.6.dylib libusbmuxd-2.0.6.dylib
 install_name_tool -change /usr/local/lib/libimobiledevice-1.0.6.dylib @executable_path/../Frameworks/libimobiledevice-1.0.6.dylib libimobiledevice-glue-1.0.0.dylib
-install_name_tool -change /usr/local/lib/libimobiledevice-1.0.6.dylib @executable_path/../Frameworks/libimobiledevice-1.0.6.dylib libplist-2.0.3.dylib
+install_name_tool -change /usr/local/lib/libimobiledevice-1.0.6.dylib @executable_path/../Frameworks/libimobiledevice-1.0.6.dylib libplist-2.0.4.dylib
 install_name_tool -change /usr/local/lib/libusbmuxd-2.0.6.dylib @executable_path/../Frameworks/libusbmuxd-2.0.6.dylib libimobiledevice-1.0.6.dylib
 install_name_tool -change /usr/local/lib/libusbmuxd-2.0.6.dylib @executable_path/../Frameworks/libusbmuxd-2.0.6.dylib libusbmuxd-2.0.6.dylib
 install_name_tool -change /usr/local/lib/libusbmuxd-2.0.6.dylib @executable_path/../Frameworks/libusbmuxd-2.0.6.dylib libimobiledevice-glue-1.0.0.dylib
-install_name_tool -change /usr/local/lib/libusbmuxd-2.0.6.dylib @executable_path/../Frameworks/libusbmuxd-2.0.6.dylib libplist-2.0.3.dylib
+install_name_tool -change /usr/local/lib/libusbmuxd-2.0.6.dylib @executable_path/../Frameworks/libusbmuxd-2.0.6.dylib libplist-2.0.4.dylib
 install_name_tool -change /usr/local/lib/libimobiledevice-glue-1.0.0.dylib @executable_path/../Frameworks/libimobiledevice-glue-1.0.0.dylib libimobiledevice-1.0.6.dylib
 install_name_tool -change /usr/local/lib/libimobiledevice-glue-1.0.0.dylib @executable_path/../Frameworks/libimobiledevice-glue-1.0.0.dylib libusbmuxd-2.0.6.dylib
 install_name_tool -change /usr/local/lib/libimobiledevice-glue-1.0.0.dylib @executable_path/../Frameworks/libimobiledevice-glue-1.0.0.dylib libimobiledevice-glue-1.0.0.dylib
-install_name_tool -change /usr/local/lib/libimobiledevice-glue-1.0.0.dylib @executable_path/../Frameworks/libimobiledevice-glue-1.0.0.dylib libplist-2.0.3.dylib
-install_name_tool -change /usr/local/lib/libplist-2.0.3.dylib @executable_path/../Frameworks/libplist-2.0.3.dylib libimobiledevice-1.0.6.dylib
-install_name_tool -change /usr/local/lib/libplist-2.0.3.dylib @executable_path/../Frameworks/libplist-2.0.3.dylib libusbmuxd-2.0.6.dylib
-install_name_tool -change /usr/local/lib/libplist-2.0.3.dylib @executable_path/../Frameworks/libplist-2.0.3.dylib libimobiledevice-glue-1.0.0.dylib
-install_name_tool -change /usr/local/lib/libplist-2.0.3.dylib @executable_path/../Frameworks/libplist-2.0.3.dylib libplist-2.0.3.dylib
+install_name_tool -change /usr/local/lib/libimobiledevice-glue-1.0.0.dylib @executable_path/../Frameworks/libimobiledevice-glue-1.0.0.dylib libplist-2.0.4.dylib
+install_name_tool -change /usr/local/lib/libplist-2.0.4.dylib @executable_path/../Frameworks/libplist-2.0.4.dylib libimobiledevice-1.0.6.dylib
+install_name_tool -change /usr/local/lib/libplist-2.0.4.dylib @executable_path/../Frameworks/libplist-2.0.4.dylib libusbmuxd-2.0.6.dylib
+install_name_tool -change /usr/local/lib/libplist-2.0.4.dylib @executable_path/../Frameworks/libplist-2.0.4.dylib libimobiledevice-glue-1.0.0.dylib
+install_name_tool -change /usr/local/lib/libplist-2.0.4.dylib @executable_path/../Frameworks/libplist-2.0.4.dylib libplist-2.0.4.dylib
