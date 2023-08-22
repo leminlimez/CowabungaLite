73bb70a70d3ac56fb387d34ac31de22e24209983
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-08-22 14:42:58 -0400

    remove all / deep clean + reorganization

--- Cowabunga Lite/Views/Main/ApplyView.swift
+++ Cowabunga Lite/Views/Main/ApplyView.swift
@@ -15,6 +15,7 @@ struct ApplyView: View {
     var body: some View {
         List {
             Text("1. PLEASE make sure you have made a backup beforehand JUST IN CASE.\n2. Disable Find My iPhone before applying. You may re-enable it after.\n3. Check the log after applying for any issues. It should say \"Restore Successful\" at the bottom if successful.")
+            Text("If you get error 205 relating to downloading files, try setting your device's language to English temporarily.")
             Text("Enabled tweaks: \(dataSingleton.enabledTweaks.isEmpty ? "None" : "")")
             ForEach(Array(dataSingleton.enabledTweaks), id: \.self) { tweak in
                 HStack(spacing: 5) {
@@ -25,21 +26,53 @@ struct ApplyView: View {
                 }
             }
             
-            NiceButton(text: AnyView(
-                HStack {
-                    Image(systemName: "checkmark.circle")
-                    Text("Apply Tweaks")
-                }
-            )) {
-                if canApply {
-                    canApply = false
-                    Task {
-                        applyTweaks()
-                        canApply = true
+            HStack {
+                // MARK: Regular Apply Button
+                NiceButton(text: AnyView(
+                    HStack {
+                        Image(systemName: "checkmark.circle")
+                        Text("Apply Tweaks")
+                    }
+                )) {
+                    if canApply {
+                        canApply = false
+                        Task {
+                            applyTweaks()
+                            canApply = true
+                        }
                     }
                 }
-            }
-            .disabled(!canApply)
+                // MARK: Remove All Tweaks
+                NiceButton(text: AnyView(
+                    HStack {
+                        Image(systemName: "trash")
+                        Text("Remove All Tweaks")
+                    }
+                )) {
+                    if canApply {
+                        canApply = false
+                        Task {
+                            removeTweaks(deepClean: false)
+                            canApply = true
+                        }
+                    }
+                }
+                // MARK: Deep Clean
+                NiceButton(text: AnyView(
+                    HStack {
+                        Image(systemName: "paintbrush")
+                        Text("Deep Clean")
+                    }
+                )) {
+                    if canApply {
+                        canApply = false
+                        Task {
+                            removeTweaks(deepClean: true)
+                            canApply = true
+                        }
+                    }
+                }
+            }.disabled(!canApply)
             TextEditor(text: .constant(logger.logText))
                 .font(Font.system(.body, design: .monospaced))
                 .frame(height: 250)
