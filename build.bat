cea8a49697d2f0e81ea936a5ccc6e60b53956bbe
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-10-27 08:30:43 -0400

    remove unnecessary files

--- build.bat
+++
@@ -1,3 +0,0 @@
-set SDKROOT=C:\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk
-set SWIFTFLAGS=-sdk %SDKROOT% -I %SDKROOT%\usr\lib\swift -L %SDKROOT%\usr\lib\swift\windows
-swiftc -o CowabungaLite.exe package.swift %SWIFTFLAGS%
